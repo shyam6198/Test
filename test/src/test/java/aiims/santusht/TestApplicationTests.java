b0b53021436cfa6e3014ee16b50c0267059b9ffd
shyam <mailtoshyamkc@gmail.com>
2025-07-11 13:56:51 +0530

    First Commit

---
+++ test/src/test/java/aiims/santusht/TestApplicationTests.java
@@ -0,0 +1,13 @@
+package aiims.santusht;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
